5b0c142596a3070b3f9ca752c71938d787bc38b3
cy <492713134@qq.com>
2018-07-12 15:17:33 +0800

    增加注释

--- mysql_optimistic.php
+++ mysql_optimistic.php
@@ -1,7 +1,11 @@
 <?php
 
 /**
+ * @author cy
+ * @since  2018-07-11
  * 使用Mysql乐观锁
+ * 优点: 乐观锁在不发生取锁失败的情况下开销比悲观锁小，程序实现，不会存在死锁等问题。
+ * 缺点: 如果取锁失败需要回滚则开销较大
  */
 
 require 'pdoClass.php';
@@ -14,7 +18,7 @@ $sql = 'update prize set num=num-1 where num=:num and num>0 ;';
 $res = $pdo->PtmQuery($sql, array('num' => $now_num));
 
 if (!empty($res['errmsg'])) {
-    echo "中奖";
+    echo "success";
 } else {
-    echo "未中奖";
+    echo "fail";
 }
