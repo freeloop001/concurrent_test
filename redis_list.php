37b937a4c0a8a5d76b42ab1f9ea2ef4f446f0ff0
cy <492713134@qq.com>
2018-07-12 17:57:13 +0800

    调整格式

--- redis_list.php
+++ redis_list.php
@@ -10,19 +10,17 @@ $redis->connect('127.0.0.1', 6379);
 
 // 比如需要抢购3个奖品
 
-// 首先在redis队列添加3个奖品码 
-$prize_code = ['aaa','bbb','ccc'];
+// 首先在redis队列添加3个奖品码
+$prize_code = ['aaa', 'bbb', 'ccc'];
 foreach ($prize_code as $key => $value) {
-	$redis->lpush( 'prize_code' , $value );
+    $redis->lpush('prize_code', $value);
 }
 
 // 用户从这里进来
-
-$code = $redis->lpop( 'prize_code' );
-if ( empty($code) ) {
-	//抢购活动已经结束，下次再来吧
-}
-else {
-	// 添加code和用户信息到mysql存储
-	// 恭喜你中奖了
+$code = $redis->lpop('prize_code');
+if (empty($code)) {
+    //抢购活动已经结束，下次再来吧
+} else {
+    // 添加code和用户信息到mysql存储
+    // 恭喜你中奖了
 }
